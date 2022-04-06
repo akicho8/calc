41867fdbf688b87f79cf54c858ca18d22d6e62eb
akicho8 <akicho8@gmail.com>
2022-04-06 14:44:26 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ~/src
+nodenv install 14.0.0
+nodenv rehash
+nodenv global 14.0.0
+node -v
+npm i -g npm
+npx create-react-app react-calc --template typescript
+cd react-calc
+npm start
